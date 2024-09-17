9e817d9a14d50cbb16548ad43bf7dcdca4594d71
Atharva Dumbre <atharva.dumbre1@gmail.com>
2024-09-17 10:15:19 -0400

    Pipeline complete

--- README.md
+++
@@ -1,2 +0,0 @@
-# Realtime-Data-Streaming
-Data Engineering End-to-End Project
--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -1,30 +1,31 @@
+import uuid
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
 default_args = {
-    'owner' : 'atharva-dumbre',
-    'start_date' : datetime(2024, 9, 14, 11, 00)
+    'owner': 'atharvadumbre',
+    'start_date': datetime(2024, 9, 16, 8, 15)
 }
 
-def get_data ():
+def get_data():
     import requests
 
     res = requests.get("https://randomuser.me/api/")
     res = res.json()
     res = res['results'][0]
-    
+
     return res
 
 def format_data(res):
     data = {}
     location = res['location']
+    #data['id'] = uuid.uuid4()
     data['first_name'] = res['name']['first']
     data['last_name'] = res['name']['last']
     data['gender'] = res['gender']
     data['address'] = f"{str(location['street']['number'])} {location['street']['name']}, " \
                       f"{location['city']}, {location['state']}, {location['country']}"
-
     data['post_code'] = location['postcode']
     data['email'] = res['email']
     data['username'] = res['login']['username']
@@ -40,28 +41,28 @@ def stream_data():
     from kafka import KafkaProducer
     import time
     import logging
-    
-    producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms = 5000)
+
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'], max_block_ms=5000, api_version=(0,10,1))
     curr_time = time.time()
 
     while True:
-        if time.time() > curr_time + 60:
+        if time.time() > curr_time + 60: #1 minute
             break
         try:
             res = get_data()
             res = format_data(res)
-            producer.send('users_created', json.dumps(res).encode('utf-8'))
 
+            producer.send('users_created', json.dumps(res).encode('utf-8'))
         except Exception as e:
-            logging.error(f'An Error Occured: {e}')
+            logging.error(f'An error occured: {e}')
             continue
 
-with DAG('user_automation', 
+with DAG('user_automation',
          default_args=default_args,
-         schedule_interval='@daily',
+         schedule='@daily',
          catchup=False) as dag:
 
     streaming_task = PythonOperator(
-        task_id = 'stream_data_from_api',
-        python_callable= stream_data
+        task_id='stream_data_from_api',
+        python_callable=stream_data
     )
